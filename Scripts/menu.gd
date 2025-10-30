7b371999ecaea4dd62ee8276056773c17aa60078
AmilkarGN <guamilkar@gmail.com>
2025-10-30 18:47:06 -0400

    Menu con funciones
    Agregue las funciones de jugar, opciones, salir y algunas posibles imagenes para parallax

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,14 +3,11 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	pass # Replace with function body.
-
-
+	get_tree().change_scene_to_file("res://Escenas/juego.tscn")
+	
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://Escenas/Opciones.tscn")
 
 func _on_exit_pressed() -> void:
-	pass # Replace with function body.
 
 	get_tree().quit()
