78664cb0a74da499b69381c4030e4e92f6ad4495
AmilkarGN <guamilkar@gmail.com>
2025-10-30 22:49:46 -0400

    actualizacion de archivos y logicas de mapas
    Se esta implementando las logicas en el primer mundo y transicion al map 1

--- Menu de inicio/Scripts/menu.gd
+++ Menu de inicio/Scripts/menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Menu de inicio/Escenas/juego.tscn")
+	get_tree().change_scene_to_file("res://Primer_mundo.tscn")
 	
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menu de inicio/Escenas/Opciones.tscn")
